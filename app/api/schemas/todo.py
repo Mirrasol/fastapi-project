29b247cfad254a736a1d6e80fbb86ca6012a8f22
Elena Dobruk <elena.dobruk@yandex.ru>
2024-12-04 13:10:34 +0300

    add ToDo pydantic model

---
+++ app/api/schemas/todo.py
@@ -0,0 +1,12 @@
+import datetime
+from pydantic import BaseModel
+
+
+class ToDoCreate(BaseModel):
+    description: str
+    completed: bool | None = False
+
+
+class ToDoFromDB(ToDoCreate):
+    id: int
+    created_at: datetime.datetime
