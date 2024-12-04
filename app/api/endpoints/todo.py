b2ffcdf7232a26b593e5bb6aa714a4f4ee288f69
Elena Dobruk <elena.dobruk@yandex.ru>
2024-12-04 15:57:02 +0300

    add todo repositories

--- app/api/endpoints/todo.py
+++ app/api/endpoints/todo.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.api.schemas.todo import ToDoCreate, ToDoFromDB
+from app.db.database import get_async_session
+from app.repositories.todo_repository import ToDoRepository, SqlAlchemyToDoRepository
 
 todo_router = APIRouter(
     prefix="/todo",
@@ -6,11 +10,15 @@ todo_router = APIRouter(
 )
 
 
-@todo_router.get("/")
-async def get_todos():
-    pass
+async def get_todo_repository(session: AsyncSession = Depends(get_async_session)) -> ToDoRepository:
+    return SqlAlchemyToDoRepository(session)
 
 
-@todo_router.post("/")
-async def create_todo():
-    pass
+@todo_router.get("/", response_model=list[ToDoFromDB])
+async def get_todos(repo: ToDoRepository = Depends(get_todo_repository)):
+    return await repo.get_todos()
+
+
+@todo_router.post("/", response_model=ToDoFromDB)
+async def create_todo(todo: ToDoCreate, repo: ToDoRepository = Depends(get_todo_repository)):
+    return await repo.create_todo(todo)
--- app/db/database.py
+++ app/db/database.py
@@ -12,3 +12,8 @@ async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
 
 class Base(DeclarativeBase):
     pass
+
+
+async def get_async_session():
+    async with async_session_maker as session:
+        yield session
